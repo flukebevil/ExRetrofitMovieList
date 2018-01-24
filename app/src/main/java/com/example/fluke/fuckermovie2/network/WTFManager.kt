ee1707797744074ac70005b595ffbdb59ce2683a
Fluke <flukebevil@gmail.com>
2018-01-24 12:07:43 +0700

    change layout fuck_item single line text use color transpan

--- app/src/main/java/com/example/fluke/fuckermovie2/network/WTFManager.kt
+++ app/src/main/java/com/example/fluke/fuckermovie2/network/WTFManager.kt
@@ -1,24 +1,16 @@
 package com.example.fluke.fuckermovie2.network
 
-import android.util.Log
-import retrofit2.Call
-import retrofit2.http.GET
-import com.example.fluke.fuckermovie2.model.MovieList
-import com.example.fluke.fuckermovie2.model.VidListFOF
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Query
 
 class WTFManager {
 
     companion object Factory {
         var serviceWTF: BaseService? = null
-        fun create(baseurl: String): BaseService? {
+        fun create(baseUrl: String): BaseService? {
             serviceWTF = Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(baseurl)
+                    .baseUrl(baseUrl)
                     .build().create(BaseService::class.java)
             return serviceWTF as BaseService?
         }
